a1ec3c7d64c5a91b2f9f17e9c806693dc199354b
Wladimir <slunam@isl.gob.cl>
2024-08-06 17:40:18 -0400

    Se agrega pipeline

--- app-cdk/lib/pipeline-cdk-stack.ts
+++ app-cdk/lib/pipeline-cdk-stack.ts
@@ -1,7 +1,9 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
-
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
+import * as codepipeline_actions from 'aws-cdk-lib/aws-codepipeline-actions';
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
 
 export class PipelineCdkStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps) {
@@ -10,6 +12,52 @@ export class PipelineCdkStack extends Stack {
     const pipeline = new codepipeline.Pipeline(this, 'Pipeline', {
         pipelineName: 'CICD_Pipeline',
     });
+
+    const codeBuild = new codebuild.PipelineProject(this, 'CodeBuild', {
+        environment: {
+          buildImage: codebuild.LinuxBuildImage.STANDARD_7_0,
+          privileged: true,
+          computeType: codebuild.ComputeType.LARGE,
+        },
+        buildSpec: codebuild.BuildSpec.fromSourceFilename('buildspec_test.yml'),
+    });
+
+    const sourceOutput = new codepipeline.Artifact();
+    const unitTestOutput = new codepipeline.Artifact();
+
+    const githubSecret = secretsmanager.Secret.fromSecretNameV2(
+        this,
+        'GitHubToken',
+        'github/personal_access_token2'
+    );
+
+    pipeline.addStage({
+        stageName: 'Source',
+        actions: [
+          new codepipeline_actions.GitHubSourceAction({
+            actionName: 'GitHub_Source',
+            owner: 'WladimirLuna',
+            repo: 'aws-usach-u2',
+            branch: 'main',
+            oauthToken: githubSecret.secretValueFromJson('github_token'),
+            output: sourceOutput
+          }),
+        ],
+    });
+
+    pipeline.addStage({
+        stageName: 'Code-Quality-Testing',
+        actions: [
+          new codepipeline_actions.CodeBuildAction({
+            actionName: 'Unit-Test',
+            project: codeBuild,
+            input: sourceOutput,
+            outputs: [unitTestOutput],
+          }),
+        ],
+    });
+  
+  
 
   }
   
