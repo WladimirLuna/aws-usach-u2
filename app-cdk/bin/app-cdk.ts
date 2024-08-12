c655e393942299c55e28c519c7398f5ac4f4734b
Wladimir <slunam@isl.gob.cl>
2024-08-12 18:04:31 -0400

    Se agrega Test Deployment

--- app-cdk/bin/app-cdk.ts
+++ app-cdk/bin/app-cdk.ts
@@ -22,4 +22,5 @@ const testCdkStack = new AppCdkStack(app, 'test', {
 const pipelineCdkStack = new PipelineCdkStack(app, 'pipeline-stack', {
     ecrRepository: ecrCdkStack.repository,
     s3Bucket: s3Bucket,
+    fargateServiceTest: testCdkStack.fargateService,
 });
